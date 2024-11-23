fbccc00c578e38e600cf7ac03e4f4f6949cac8c0
Igor Rusakov <rusakovigor66@gmail.com>
2024-11-23 22:09:33 +0300

    Commit

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Coppyright Igor Rusakov
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
