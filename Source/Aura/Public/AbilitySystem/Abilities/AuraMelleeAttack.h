622af9bd9a9d575b1c986c44f3d82e6604a02706
Igor Rusakov <rusakovigor66@gmail.com>
2024-12-21 20:53:18 +0300

    Commit

---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraMelleeAttack.h
@@ -0,0 +1,17 @@
+// Coppyright Igor Rusakov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraDamageGameplayAbility.h"
+#include "AuraMelleeAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraMelleeAttack : public UAuraDamageGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
